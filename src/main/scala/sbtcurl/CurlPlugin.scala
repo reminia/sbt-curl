196f0dbab7846b2894cd1100b3f3a66794339e23
sleefd <sleefd@gmail.com>
2023-12-08 18:22:10 +0800

    log with sbt task streams

--- src/main/scala/sbtcurl/CurlPlugin.scala
+++ src/main/scala/sbtcurl/CurlPlugin.scala
@@ -17,10 +17,12 @@ object CurlPlugin extends AutoPlugin {
 
   import autoImport._
 
+
   lazy val curlSettings: Seq[Def.Setting[_]] = Seq(
     curl := {
       val input = Def.spaceDelimited().parsed.mkString(" ")
-      println(Curl(input))
+      implicit val log: Logger = streams.value.log
+      log.info(Curl(input))
     },
   )
 
@@ -40,10 +42,11 @@ object CurlPlugin extends AutoPlugin {
     },
     curlTest := {
       val someFile = curlTestScript.value
+      implicit val log: Logger = streams.value.log
       if (someFile.isDefined) {
         Curl(someFile.get).foreach(println)
       } else {
-        println("No any curl script file found!")
+        log.error("No any curl script file found!")
       }
     }
   )
@@ -55,11 +58,16 @@ object CurlPlugin extends AutoPlugin {
 }
 
 object Curl {
-  def apply(cmd: String): String = {
-    s"curl $cmd".!!
+  def apply(cmd: String)(implicit logger: Logger): String = {
+    var curl = cmd
+    if(!cmd.startsWith("curl")) {
+      curl = s"curl $cmd"
+    }
+    logger.info(curl)
+    curl.!!
   }
 
-  def apply(file: File): Seq[String] = {
+  def apply(file: File)(implicit logger: Logger): Seq[String] = {
     val source = Source.fromFile(file)
     try {
       val (list, _) = source
@@ -67,15 +75,15 @@ object Curl {
         // remove empty and comment line
         .filter(line => line.nonEmpty && !line.startsWith("#"))
         .foldRight[(List[String], List[String])](List(), List()) { case (line, (list1, list2)) =>
-          if (line.dropWhile(_.isWhitespace).startsWith("curl")) {
-            val s = (line :: list2).mkString(System.lineSeparator())
+          val _line = line.dropWhile(_.isWhitespace)
+          if (_line.startsWith("curl")) {
+            val s = (_line :: list2).mkString(System.lineSeparator())
             (s :: list1) -> List()
           } else {
-            list1 -> (line :: list2)
+            list1 -> (_line :: list2)
           }
         }
-      list.foreach(x => println("cmd: " + x))
-      list.map(cmd => Curl(cmd))
+      list.map(Curl.apply)
     } finally {
       source.close()
     }
